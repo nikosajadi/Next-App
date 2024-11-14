576c862a97e8f09acb4b637b4474f9108d260ee9
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-11-14 12:11:52 +0100

    [Update] coustom App and documents from nextJs

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { ThemeProvider } from '@emotion/react';
+import type { AppProps } from 'next/app'
+ import { theme } from './configs/theme';
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+  <ThemeProvider theme={theme} >
+  <Component {...pageProps} />
+  </ThemeProvider>)
+}
---
+++ src/pages/configs/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  primary: 'blue',
+  secondary: 'gray',
+  typography: {
+    h1: {
+      fontSize: 30,
+    },
+  },
+};
+
+export default theme;
