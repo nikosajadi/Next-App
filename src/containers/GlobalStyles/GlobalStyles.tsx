c8b8f942da451df12e1cead4589065afdd519102
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-11-18 17:01:42 +0100

    [Update] add Reset css

--- src/containers/GlobalStyles/GlobalStyles.tsx
+++ src/containers/GlobalStyles/GlobalStyles.tsx
@@ -5,9 +5,58 @@ export default function GlobalStyles() {
   return (
     <Global
       styles={css`
-        *, *::before, *::after {
+        /* Reset Styles */
+        html, body, div, span, applet, object, iframe,
+        h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+        a, abbr, acronym, address, big, cite, code,
+        del, dfn, em, img, ins, kbd, q, s, samp,
+        small, strike, strong, sub, sup, tt, var,
+        b, u, i, center,
+        dl, dt, dd, ol, ul, li,
+        fieldset, form, label, legend,
+        table, caption, tbody, tfoot, thead, tr, th, td,
+        article, aside, canvas, details, embed,
+        figure, figcaption, footer, header, hgroup,
+        menu, nav, output, ruby, section, summary,
+        time, mark, audio, video {
           margin: 0;
           padding: 0;
+          border: 0;
+          font-size: 100%;
+          font: inherit;
+          vertical-align: baseline;
+        }
+
+        /* HTML5 display-role reset for older browsers */
+        article, aside, details, figcaption, figure,
+        footer, header, hgroup, menu, nav, section {
+          display: block;
+        }
+
+        body {
+          line-height: 1;
+        }
+
+        ol, ul {
+          list-style: none;
+        }
+
+        blockquote, q {
+          quotes: none;
+        }
+
+        blockquote:before, blockquote:after,
+        q:before, q:after {
+          content: '';
+        }
+
+        table {
+          border-collapse: collapse;
+          border-spacing: 0;
+        }
+
+        /* Custom Global Styles */
+        *, *::before, *::after {
           box-sizing: border-box;
         }
 
@@ -24,6 +73,8 @@ export default function GlobalStyles() {
 
         button {
           cursor: pointer;
+          border: none;
+          background: none;
         }
       `}
     />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
- 
+import GlobalStyles from '@/containers/GlobalStyles/GlobalStyles';
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <GlobalStyles />
         <Main />
         <NextScript />
       </body>
