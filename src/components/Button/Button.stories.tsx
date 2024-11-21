96ea75f3b2f3fadf7419f789527e40dd966d0fa8
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-11-21 20:55:42 +0100

    [Update] fix storybook webpack

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import Button from './Button';
 
-
 export default { title: 'Button' };
 
-export const simple = () => <Button>Click me</Button>;
-
-export const simpleWidthAll = () => <Button widthAll>Click me</Button>; // Pass `widthAll`
+export const Simple = () => <Button>Click me</Button>;
+export const SimpleWithAll = () => <Button>Click me</Button>; // Adjust the props as necessary
